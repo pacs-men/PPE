b9f075da216405ca3610a6d3c2b05d3565d12d88
pacs-men <cemeric22@gmail.com>
2017-05-19 09:51:40 +0200

    Update test_moteur_nv_carte_puissance.ino

--- test_moteur_nv_carte_puissance/test_moteur_nv_carte_puissance.ino
+++ test_moteur_nv_carte_puissance/test_moteur_nv_carte_puissance.ino
@@ -26,6 +26,53 @@ void loop(){
   analogWrite(E2, 100);
   //PWM Speed Control
   delay(3000);
-  
-  
+ 
+}
+
+void avancer(int temp){
+  digitalWrite(M1,HIGH);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, 100);
+  //PWM Speed Control
+  analogWrite(E2, 100);
+  //PWM Speed Control
+  delay(temp);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+}
+
+void reculer(int temp){
+  digitalWrite(M1,LOW);
+  digitalWrite(M2, LOW);
+  analogWrite(E1, 100);
+  //PWM Speed Control
+  analogWrite(E2, 100);
+  //PWM Speed Control
+  delay(temp);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+}
+
+void tournerG(int temp){
+  digitalWrite(M1,LOW);
+  digitalWrite(M2, HIGH);
+  analogWrite(E1, 100);
+  //PWM Speed Control
+  analogWrite(E2, 100);
+  //PWM Speed Control
+  delay(temp);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
+}
+
+void tournerD(int temp){
+  digitalWrite(M1,HIGH);
+  digitalWrite(M2, LOW);
+  analogWrite(E1, 100);
+  //PWM Speed Control
+  analogWrite(E2, 100);
+  //PWM Speed Control
+  delay(temp);
+  analogWrite(E1, 0);
+  analogWrite(E2, 0);
 }
